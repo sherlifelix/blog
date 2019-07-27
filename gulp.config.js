12416898cf6ea38aa8a003bc7bca2f8337ff8512
Marappan <Chandrasekar_Marappa@Dell.com>
2019-07-27 23:07:41 +0530

    image resize

--- gulp.config.js
+++ gulp.config.js
@@ -1,31 +1,27 @@
-module.exports = function () {
-
-var root = './';
- var nodeModules = 'node_modules';
-var source='src';
-var assets='_assets';
-var temp='tmp';
- var config = {
-     temp:temp,
-     build: './assets',
-     source: source,
-     css: assets + '/css/',
-     fonts: assets + '/fonts/*.*',
-     html: source + '/**/*.html',
-     images: assets + '/images/**/**/**/*.*',
-     index: source + '/index.html',
-     sass: assets + '/css/pages/_sass/*.scss',
-     js:assets + '/js',
+module.exports = function() {
+  var root = "./";
+  var nodeModules = "node_modules";
+  var source = "src";
+  var assets = "_assets";
+  var temp = "tmp";
+  var config = {
+    temp: temp,
+    build: "./assets",
+    source: source,
+    css: assets + "/css/",
+    fonts: assets + "/fonts/*.*",
+    html: source + "/**/*.html",
+    images: assets + "/images/**/*.{jpg,png,jpeg}",
+    index: source + "/index.html",
+    sass: assets + "/css/pages/_sass/*.scss",
+    js: assets + "/js",
     /**
      * browser sync
      */
     browserReloadDelay: 1000,
-      packages: [
-      './package.json'
-    ],
-    deploy:'./_site'
- }
+    packages: ["./package.json"],
+    deploy: "./_site"
+  };
 
- return config;
-
-}
+  return config;
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
-var args = require('yargs').argv;
-var browserSync = require('browser-sync');
-var config = require('./gulp.config')();
-var gulp = require('gulp');
-var path = require('path');
-var del = require('del');
-var gh_deploy = require('gulp-gh-pages');
+var args = require("yargs").argv;
+var browserSync = require("browser-sync");
+var config = require("./gulp.config")();
+var gulp = require("gulp");
+var path = require("path");
+var del = require("del");
+var gh_deploy = require("gulp-gh-pages");
 
-var $ = require('gulp-load-plugins')({
+var $ = require("gulp-load-plugins")({
   lazy: true
 });
 
@@ -14,8 +14,8 @@ var colors = $.util.colors;
 /**
  * Push build to gh-pages
  */
-gulp.task('deploy', function() {
-  return gulp.src('./dist/**/*').pipe(gh_deploy());
+gulp.task("deploy", function() {
+  return gulp.src("./dist/**/*").pipe(gh_deploy());
 });
 
 var deploy = config.deploy;
@@ -23,19 +23,19 @@ var deploy = config.deploy;
  * vet the code and create coverage report
  * @return {Stream}
  */
-gulp.task('vet', function() {
-  log('Analyzing source with JSHint and JSCS');
+gulp.task("vet", function() {
+  log("Analyzing source with JSHint and JSCS");
 
   return gulp
     .src(config.alljs)
     .pipe($.if(args.verbose, $.print()))
     .pipe($.jshint())
     .pipe(
-      $.jshint.reporter('jshint-stylish', {
+      $.jshint.reporter("jshint-stylish", {
         verbose: true
       })
     )
-    .pipe($.jshint.reporter('fail'));
+    .pipe($.jshint.reporter("fail"));
   //.pipe($.jscs());
 });
 
@@ -44,39 +44,39 @@ gulp.task('vet', function() {
  * @return {Stream} ['clean-styles']
  */
 
-gulp.task('page-styles', function() {
-  log('Compiling Sass --> CSS');
+gulp.task("page-styles", function() {
+  log("Compiling Sass --> CSS");
 
   return (
     gulp
       .src(config.sass)
       .pipe($.plumber()) // exit gracefully if something fails after this
-      .pipe($.sass().on('error', $.sass.logError))
+      .pipe($.sass().on("error", $.sass.logError))
       // more verbose and dupe output. requires emit.
       .pipe(
         $.autoprefixer({
-          browsers: ['last 2 version', '> 5%']
+          browsers: ["last 2 version", "> 5%"]
         })
       )
-      .pipe(gulp.dest(config.temp + '/css/'))
+      .pipe(gulp.dest(config.temp + "/css/"))
   );
 });
 
-gulp.task('vendor-styles', function() {
-  log('Compiling vendor styles');
+gulp.task("vendor-styles", function() {
+  log("Compiling vendor styles");
 
   return gulp
-    .src(config.css + '/vendor/**/*.css')
-    .pipe($.concat('vendor.min.css'))
-    .pipe(gulp.dest(config.temp + '/css/'));
+    .src(config.css + "/vendor/**/*.css")
+    .pipe($.concat("vendor.min.css"))
+    .pipe(gulp.dest(config.temp + "/css/"));
 });
 
-gulp.task('temp-styles', ['page-styles', 'vendor-styles']);
+gulp.task("temp-styles", ["page-styles", "vendor-styles"]);
 
-gulp.task('dev-styles', ['temp-styles'], function() {
+gulp.task("dev-styles", ["temp-styles"], function() {
   return gulp
-    .src(config.temp + '/css/**/*.css')
-    .pipe(gulp.dest(config.build + '/css'));
+    .src(config.temp + "/css/**/*.css")
+    .pipe(gulp.dest(config.build + "/css"));
   // .pipe(browserSync.reload({stream:true, once: true}));;
 });
 
@@ -85,29 +85,29 @@ gulp.task('dev-styles', ['temp-styles'], function() {
  * @return {Stream} ['clean-styles']
  */
 
-gulp.task('page-scripts', function() {
-  log('assets js --> temp js');
+gulp.task("page-scripts", function() {
+  log("assets js --> temp js");
 
   return gulp
-    .src(config.js + '/pages/*.js')
-    .pipe(gulp.dest(config.temp + '/js/pages/'));
+    .src(config.js + "/pages/*.js")
+    .pipe(gulp.dest(config.temp + "/js/pages/"));
 });
 
-gulp.task('vendor-scripts', function() {
-  log('assets vendor js --> temp vendor js');
+gulp.task("vendor-scripts", function() {
+  log("assets vendor js --> temp vendor js");
 
   return gulp
-    .src(config.js + '/vendor/*.js')
-    .pipe($.concat('vendor.js'))
-    .pipe(gulp.dest(config.temp + '/js/vendor/'));
+    .src(config.js + "/vendor/*.js")
+    .pipe($.concat("vendor.js"))
+    .pipe(gulp.dest(config.temp + "/js/vendor/"));
 });
 
-gulp.task('temp-scripts', ['page-scripts', 'vendor-scripts']);
+gulp.task("temp-scripts", ["page-scripts", "vendor-scripts"]);
 
-gulp.task('dev-scripts', ['temp-scripts'], function() {
+gulp.task("dev-scripts", ["temp-scripts"], function() {
   return gulp
-    .src(config.temp + '/js/**/*.js')
-    .pipe(gulp.dest(config.build + '/js'));
+    .src(config.temp + "/js/**/*.js")
+    .pipe(gulp.dest(config.build + "/js"));
   //.pipe(browserSync.reload({stream:true, once: true}));;
 });
 
@@ -115,27 +115,36 @@ gulp.task('dev-scripts', ['temp-scripts'], function() {
  * Copy fonts
  * @return {Stream}
  */
-gulp.task('fonts', function() {
-  log('Copying fonts');
+gulp.task("fonts", function() {
+  log("Copying fonts");
 
-  return gulp.src(config.fonts).pipe(gulp.dest(config.build + '/font/'));
+  return gulp.src(config.fonts).pipe(gulp.dest(config.build + "/font/"));
 });
 
 /**
  * Compress images
  * @return {Stream}
  */
-gulp.task('images', function() {
-  log('Compressing and copying images');
+gulp.task("images", function() {
+  log("Compressing and copying images");
 
-  return (
-    gulp
-      .src(config.images)
-      // .pipe($.imagemin({
-      //   optimizationLevel: 4
-      // }))
-      .pipe(gulp.dest(config.build + '/img/'))
-  );
+  return gulp
+    .src(config.images)
+    .pipe($.plumber())
+    .pipe(
+      $.imagemin({
+        optimizationLevel: 4
+      })
+    )
+    .pipe(
+      $.imageResize({
+        width: 600,
+        height: 600,
+        crop: true,
+        upscale: false
+      })
+    )
+    .pipe(gulp.dest(config.build + "/img/"));
 });
 
 /**
@@ -143,13 +152,13 @@ gulp.task('images', function() {
  * This is separate so we can run tests on
  * optimize before handling image or fonts
  */
-gulp.task('build', ['build-css', 'build-js', 'fonts', 'images'], function() {
-  log('Building everything');
+gulp.task("build", ["build-css", "build-js", "fonts", "images"], function() {
+  log("Building everything");
 
   var msg = {
-    title: 'gulp build',
-    subtitle: 'Deployed to the build folder',
-    message: 'Running `gulp serve-build`'
+    title: "gulp build",
+    subtitle: "Deployed to the build folder",
+    message: "Running `gulp serve-build`"
   };
   del(config.temp);
   log(msg);
@@ -160,50 +169,50 @@ gulp.task('build', ['build-css', 'build-js', 'fonts', 'images'], function() {
  * Remove all fonts from the build folder
  * @param  {Function} done - callback when complete
  */
-gulp.task('clean-fonts', function(done) {
-  clean(config.build + '/font/**/*.*', done);
+gulp.task("clean-fonts", function(done) {
+  clean(config.build + "/font/**/*.*", done);
 });
 
 /**
  * Remove all images from the build folder
  * @param  {Function} done - callback when complete
  */
-gulp.task('clean-images', function(done) {
-  clean(config.build + '/images/**/*.*', done);
+gulp.task("clean-images", function(done) {
+  clean(config.build + "/images/**/*.*", done);
 });
 
 /**
  * Remove all styles from the build and temp folders
  * @param  {Function} done - callback when complete
  */
-gulp.task('clean-page-styles', function(done) {
+gulp.task("clean-page-styles", function(done) {
   var files = [].concat(
-    config.temp + '/css/pages/**/*.css',
-    config.build + '/css/pages/**/*.css'
+    config.temp + "/css/pages/**/*.css",
+    config.build + "/css/pages/**/*.css"
   );
   clean(files, done);
 });
 
-gulp.task('clean-vendor-styles', function(done) {
+gulp.task("clean-vendor-styles", function(done) {
   var files = [].concat(
-    config.temp + '/css/vendor/**/*.css',
-    config.build + '/css/vendor/**/*.css'
+    config.temp + "/css/vendor/**/*.css",
+    config.build + "/css/vendor/**/*.css"
   );
   clean(files, done);
 });
 
-gulp.task('clean-page-scripts', function(done) {
+gulp.task("clean-page-scripts", function(done) {
   var files = [].concat(
-    config.temp + '/js/pages/*.js',
-    config.build + '/js/pages/*.js'
+    config.temp + "/js/pages/*.js",
+    config.build + "/js/pages/*.js"
   );
   clean(files, done);
 });
 
-gulp.task('clean-vendor-scripts', function(done) {
+gulp.task("clean-vendor-scripts", function(done) {
   var files = [].concat(
-    config.temp + '/js/vendor/*.js',
-    config.build + '/js/vendor/*.js'
+    config.temp + "/js/vendor/*.js",
+    config.build + "/js/vendor/*.js"
   );
   clean(files, done);
 });
@@ -214,7 +223,7 @@ gulp.task('clean-vendor-scripts', function(done) {
  * @param  {Function} done - callback when complete
  */
 function clean(path, done) {
-  log('Cleaning: ' + $.util.colors.blue(path));
+  log("Cleaning: " + $.util.colors.blue(path));
   del(path, done);
 }
 
@@ -223,7 +232,7 @@ function clean(path, done) {
  * Can pass in a string, object or array.
  */
 function log(msg) {
-  if (typeof msg === 'object') {
+  if (typeof msg === "object") {
     for (var item in msg) {
       if (msg.hasOwnProperty(item)) {
         $.util.log($.util.colors.blue(msg[item]));
@@ -239,53 +248,53 @@ function log(msg) {
  * and inject them into the new index.html
  * @return {Stream}
  */
-gulp.task('optimize-css', ['page-styles'], function() {
-  log('Optimizing css');
+gulp.task("optimize-css", ["page-styles"], function() {
+  log("Optimizing css");
   return gulp
-    .src(config.temp + '/css/*.css')
+    .src(config.temp + "/css/*.css")
     .pipe($.cleanCss())
-    .pipe(gulp.dest(config.build + '/css'));
+    .pipe(gulp.dest(config.build + "/css"));
 });
 
-gulp.task('optimize-vendor-css', ['vendor-styles'], function() {
-  log('Optimizing vendor css');
+gulp.task("optimize-vendor-css", ["vendor-styles"], function() {
+  log("Optimizing vendor css");
   return gulp
-    .src(config.temp + '/vendor/*.css')
-    .pipe($.concat('vendor.min.css'))
+    .src(config.temp + "/vendor/*.css")
+    .pipe($.concat("vendor.min.css"))
     .pipe($.cleanCss())
-    .pipe(gulp.dest(config.build + '/css'));
+    .pipe(gulp.dest(config.build + "/css"));
 });
 
-gulp.task('build-css', ['optimize-css', 'optimize-vendor-css']);
+gulp.task("build-css", ["optimize-css", "optimize-vendor-css"]);
 
-gulp.task('optimize-vendor-js', ['vendor-scripts'], function() {
-  log('Optimizing js');
+gulp.task("optimize-vendor-js", ["vendor-scripts"], function() {
+  log("Optimizing js");
   return gulp
-    .src(config.temp + '/js/vendor/*.js')
-    .pipe($.concat('vendor.js'))
+    .src(config.temp + "/js/vendor/*.js")
+    .pipe($.concat("vendor.js"))
     .pipe($.uglify())
-    .pipe(gulp.dest(config.build + '/js/vendor'));
+    .pipe(gulp.dest(config.build + "/js/vendor"));
 });
-gulp.task('optimize-page-js', ['page-scripts'], function() {
-  log('Optimizing js');
+gulp.task("optimize-page-js", ["page-scripts"], function() {
+  log("Optimizing js");
   return gulp
-    .src(config.temp + '/js/pages/*.js')
+    .src(config.temp + "/js/pages/*.js")
     .pipe($.uglify())
-    .pipe(gulp.dest(config.build + '/js/pages'));
+    .pipe(gulp.dest(config.build + "/js/pages"));
 });
 
-gulp.task('build-js', ['optimize-page-js', 'optimize-vendor-js']);
+gulp.task("build-js", ["optimize-page-js", "optimize-vendor-js"]);
 
 gulp.task(
-  'serve-dev-assets',
-  ['dev-styles', 'dev-scripts', 'fonts', 'images'],
+  "serve-dev-assets",
+  ["dev-styles", "dev-scripts", "fonts", "images"],
   function() {
-    log('Building everything for development');
+    log("Building everything for development");
 
     var msg = {
-      title: 'gulp dev-assets',
-      subtitle: 'Assets  build  for development',
-      message: 'Running `gulp serve-dev`'
+      title: "gulp dev-assets",
+      subtitle: "Assets  build  for development",
+      message: "Running `gulp serve-dev`"
     };
     del(config.temp);
     log(msg);
@@ -293,24 +302,24 @@ gulp.task(
   }
 );
 
-gulp.task('clean', [
-  'clean-fonts',
-  'clean-page-styles',
-  'clean-vendor-styles',
-  'clean-page-scripts',
-  'clean-vendor-scripts',
-  'clean-fonts',
-  'clean-images'
+gulp.task("clean", [
+  "clean-fonts",
+  "clean-page-styles",
+  "clean-vendor-styles",
+  "clean-page-scripts",
+  "clean-vendor-scripts",
+  "clean-fonts",
+  "clean-images"
 ]);
-gulp.task('serve-dev', ['jekyll-build'], function() {
+gulp.task("serve-dev", ["jekyll-build"], function() {
   //gulp.start('clean');
-  gulp.start('browser-sync');
+  gulp.start("browser-sync");
 });
 
-var exec = require('child_process').exec;
-gulp.task('jekyll-build', ['serve-dev-assets'], function(cb) {
+var exec = require("child_process").exec;
+gulp.task("jekyll-build", ["serve-dev-assets"], function(cb) {
   // build Jekyll
-  exec('bundle exec jekyll build', function(err) {
+  exec("bundle exec jekyll build", function(err) {
     if (err) return cb(err); // return error
     cb(); // finished task
   });
@@ -319,33 +328,33 @@ gulp.task('jekyll-build', ['serve-dev-assets'], function(cb) {
 // -->
 // Browser Sync
 // <--
-gulp.task('browser-sync', ['jekyll-build'], function() {
+gulp.task("browser-sync", ["jekyll-build"], function() {
   browserSync.init(null, {
     server: {
-      baseDir: './' + deploy
+      baseDir: "./" + deploy
     }
   });
-  gulp.watch(config.js + '/**/*.js', ['rebuild']);
-  gulp.watch(config.css + '/**/*.scss', ['rebuild']);
-  gulp.watch('./**/*.html', ['rebuild']);
+  gulp.watch(config.js + "/**/*.js", ["rebuild"]);
+  gulp.watch(config.css + "/**/*.scss", ["rebuild"]);
+  gulp.watch("./**/*.html", ["rebuild"]);
 });
 
 // -->
 // Browser Sync
 // <--
-gulp.task('browser-sync', ['jekyll-build'], function() {
+gulp.task("browser-sync", ["jekyll-build"], function() {
   browserSync.init(null, {
     server: {
-      baseDir: './' + deploy
+      baseDir: "./" + deploy
     }
   });
-  gulp.watch(config.js + '/**/*.js', ['rebuild']);
-  gulp.watch(config.css + '/**/*.scss', ['rebuild']);
-  gulp.watch('./_layouts/**/*.html', ['rebuild']);
-  gulp.watch('./**/*.md', ['rebuild']);
+  gulp.watch(config.js + "/**/*.js", ["rebuild"]);
+  gulp.watch(config.css + "/**/*.scss", ["rebuild"]);
+  gulp.watch("./_layouts/**/*.html", ["rebuild"]);
+  gulp.watch("./**/*.md", ["rebuild"]);
 });
 
-gulp.task('rebuild', ['jekyll-build'], function() {
+gulp.task("rebuild", ["jekyll-build"], function() {
   browserSync.reload();
 });
 
@@ -353,36 +362,56 @@ gulp.task('rebuild', ['jekyll-build'], function() {
 // Default task
 // <--
 
-gulp.task('default', ['serve-dev']);
+gulp.task("default", ["serve-dev"]);
 
 /**
  * Compress images
  * @return {Stream}
  */
-gulp.task('compress-images', function() {
-  log('Compressing and copying images');
+gulp.task("compress-images", function() {
+  log("Compressing and copying images");
 
   return gulp
     .src(config.images)
+    .pipe($.plumber())
     .pipe(
       $.imagemin({
         optimizationLevel: 5,
         verbose: true
       })
     )
-    .pipe(gulp.dest('_assets/op-img/'));
+    .pipe(
+      $.imageResize({
+        width: 600,
+        height: 600,
+        crop: true,
+        upscale: false
+      })
+    )
+    .pipe(gulp.dest("_assets/op-img/"));
 });
 
-gulp.task('compress-data-images', function() {
-  log('Compressing and copying images');
+gulp.task("compress-data-images", function() {
+  log("Compressing and copying images");
 
-  return gulp
-    .src('./_assets/**/**/*.*')
-    .pipe(
-      $.imagemin({
-        optimizationLevel: 5,
-        verbose: true
-      })
-    )
-    .pipe(gulp.dest('_assets/'));
+  return (
+    gulp
+      .src("./_assets/**/**/*.*")
+      .pipe($.plumber())
+      .pipe(
+        $.imagemin({
+          optimizationLevel: 5,
+          verbose: true
+        })
+      )
+      // .pipe(
+      //   $.imageResize({
+      //     width: 600,
+      //     height: 600,
+      //     crop: true,
+      //     upscale: false
+      //   })
+      // )
+      .pipe(gulp.dest("_assets/"))
+  );
 });
